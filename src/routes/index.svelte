dd51c02edd842d3fcd55f9293ea634e4c25fcd72
Nat Troyer <nathanaelatroyer@gmail.com>
2020-09-20 10:05:22 -0400

    change to sapper

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<style>
+	h1, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Welcome to my web page!</h1>
+
+<p><strong>This page is built using Svelte with Sapper</strong></p>
