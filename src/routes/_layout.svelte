a775bec9df37d17be525eeaf9170a61409b02a5a
Nat Troyer <nathanaelatroyer@gmail.com>
2020-11-01 22:31:48 -0500

    working on tsparticles implementation

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,33 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import { onMount } from "svelte";
+
+	let ParticlesComponent;
+
+	onMount(async() => {
+		const module = await import("svelte-particles");
+
+		ParticlesComponent = module.default;
+	});
+
+	let particlesConfig = {
+		particles: {
+			color: {
+				value: "#00ffff"
+			},
+			links: {
+				enable: true,
+				color: "#00ffff"
+			},
+			move: {
+				enable: true
+			},
+		}
+	};
+
+	let onParticlesLoaded = (event) => {
+		const particlesContainer = event.detail.particles;
+	};
 
 	export let segment;
 </script>
@@ -14,8 +42,10 @@
 	}
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
+<!-- The below item needs to be styled -->
+<!--<svelte:component this="{ParticlesComponent}" id="tsparticles" options="{particlesConfig}" on:particlesLoaded="{onParticlesLoaded}" />-->
 <main>
 	<slot></slot>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,7 +40,7 @@
 		font-size: 6rem;
 	}
 
-	.main-webpage {
+	.main-webpage, .contact-icons {
 		animation: 2s ease-out 0s 1 mainstart;
 	}
 	@keyframes mainstart {
@@ -50,43 +50,6 @@
 		to {
 			opacity: 1;
 		}	
-	}
-
-	.resume-icon {
-		animation: 2s ease-in 2s 1 iconstart;
-		animation-fill-mode: forwards;
-		visibility: hidden;
-	}	
-
-	.email-icon {
-		animation: 2s ease-in 2.4s 1 iconstart;
-		animation-fill-mode: forwards;
-		visibility: hidden;
-	}
-
-	.github-icon {
-		animation: 2s ease-in 2.8s 1 iconstart;
-		animation-fill-mode: forwards;
-		visibility: hidden;
-	}
-
-	.linkedin-icon {
-		animation: 2s ease-in 3.2s 1 iconstart;
-		animation-fill-mode: forwards;
-		visibility: hidden;
-	}
-
-	@keyframes iconstart {
-		from {
-			opacity: 0;
-			transform: translate3d(0, 100%, 0);
-			visibility: visible;
-		}
-		to {
-			opacity: 1;
-			transform: none;
-			visibility: visible;
-		}
 	}
 
 	@media (min-width: 480px) {
@@ -106,31 +69,29 @@
 	<title>Nat Troyer - Web Developer</title>
 </svelte:head>
 
-<main>
-	<div class="main-webpage">
-		<h1>Nat Troyer</h1>
+<div class="main-webpage">
+	<h1>Nat Troyer</h1>
 
-		<p>Full-stack Web Developer with over 6 years of experience</p>
-		<ul class="about-list">
-			<li> </li>
-			<li><a href="about">About</a></li>
-			<li><i class="material-icons">code</i></li>
-			<li><a href="projects">Projects</a></li>
-			<li> </li>
-		</ul>
-	</div>
-	<ul class="contact-icons">
-		<li class="resume-icon">
-			<a href="{process.env.RESUME_LINK}" data-tooltip="Resume"><i class="material-icons">contact_page</i></a>
-		</li>
-		<li class="email-icon">
-			<a href="{process.env.EMAIL}" data-tooltip="Email"><i class="material-icons">email</i></a>
-		</li>
-		<li class="github-icon">
-			<a href="{process.env.GITHUB_LINK}" data-tooltip="GitHub"><i class="devicon-github-plain"></i></a>
-		</li>
-		<li class="linkedin-icon">
-			<a href="{process.env.LINKEDIN_LINK}" data-tooltip="LinkedIn"><i class="devicon-linkedin-plain"></i></a>
-		</li>
+	<p>Full-stack Web Developer with over 6 years of experience</p>
+	<ul class="about-list">
+		<li> </li>
+		<li><a href="about">About</a></li>
+		<li><i class="material-icons">code</i></li>
+		<li><a href="projects">Projects</a></li>
+		<li> </li>
 	</ul>
-</main>
+</div>
+<ul class="contact-icons">
+	<li class="resume-icon">
+		<a href="{process.env.RESUME_LINK}" data-tooltip="Resume"><i class="material-icons">contact_page</i></a>
+	</li>
+	<li class="email-icon">
+		<a href="{process.env.EMAIL}" data-tooltip="Email"><i class="material-icons">email</i></a>
+	</li>
+	<li class="github-icon">
+		<a href="{process.env.GITHUB_LINK}" data-tooltip="GitHub"><i class="devicon-github-plain"></i></a>
+	</li>
+	<li class="linkedin-icon">
+		<a href="{process.env.LINKEDIN_LINK}" data-tooltip="LinkedIn"><i class="devicon-linkedin-plain"></i></a>
+	</li>
+</ul>
