822a2aa10e9224e925b43b7ec694f6fef698ae71
Nat Troyer <nathanaelatroyer@gmail.com>
2020-11-03 20:41:33 -0500

    switched to svelte animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -40,18 +40,6 @@
 		font-size: 6rem;
 	}
 
-	.main-webpage, .contact-icons {
-		animation: 2s ease-out 0s 1 mainstart;
-	}
-	@keyframes mainstart {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}	
-	}
-
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4rem;
@@ -65,33 +53,45 @@
 	}
 </style>
 
+<script>
+	import { fly } from 'svelte/transition';
+	import { onMount } from "svelte";
+
+	let ready = false;
+	onMount(() => ready = true);
+</script>
+
 <svelte:head>
 	<title>Nat Troyer - Web Developer</title>
 </svelte:head>
 
-<div class="main-webpage">
-	<h1>Nat Troyer</h1>
+{#if ready}
+	<div in:fly="{{y: 20, duration: 2000}}">
+		<div class="main-webpage">
+			<h1>Nat Troyer</h1>
 
-	<p>Full-stack Web Developer with over 6 years of experience</p>
-	<ul class="about-list">
-		<li> </li>
-		<li><a href="about">About</a></li>
-		<li><i class="material-icons">code</i></li>
-		<li><a href="projects">Projects</a></li>
-		<li> </li>
-	</ul>
-</div>
-<ul class="contact-icons">
-	<li class="resume-icon">
-		<a href="{process.env.RESUME_LINK}" data-tooltip="Resume"><i class="material-icons">contact_page</i></a>
-	</li>
-	<li class="email-icon">
-		<a href="{process.env.EMAIL}" data-tooltip="Email"><i class="material-icons">email</i></a>
-	</li>
-	<li class="github-icon">
-		<a href="{process.env.GITHUB_LINK}" data-tooltip="GitHub"><i class="devicon-github-plain"></i></a>
-	</li>
-	<li class="linkedin-icon">
-		<a href="{process.env.LINKEDIN_LINK}" data-tooltip="LinkedIn"><i class="devicon-linkedin-plain"></i></a>
-	</li>
-</ul>
+			<p>Full-stack Web Developer with over 6 years of experience</p>
+			<ul class="about-list">
+				<li> </li>
+				<li><a href="about">About</a></li>
+				<li><i class="material-icons">code</i></li>
+				<li><a href="projects">Projects</a></li>
+				<li> </li>
+			</ul>
+		</div>
+		<ul class="contact-icons">
+			<li class="resume-icon">
+				<a href="{process.env.RESUME_LINK}" data-tooltip="Resume"><i class="material-icons">contact_page</i></a>
+			</li>
+			<li class="email-icon">
+				<a href="{process.env.EMAIL}" data-tooltip="Email"><i class="material-icons">email</i></a>
+			</li>
+			<li class="github-icon">
+				<a href="{process.env.GITHUB_LINK}" data-tooltip="GitHub"><i class="devicon-github-plain"></i></a>
+			</li>
+			<li class="linkedin-icon">
+				<a href="{process.env.LINKEDIN_LINK}" data-tooltip="LinkedIn"><i class="devicon-linkedin-plain"></i></a>
+			</li>
+		</ul>
+	</div>
+{/if}
